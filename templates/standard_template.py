1c1b2487cbb9bf75db99faa6998763f19ad2d50d
Rittenhau5 <rrittenhau5@gmail.com>
2016-12-02 23:29:55 -0700

    Initial Commit

---
+++ templates/standard_template.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  name_me.py
+#  
+# Python 2.7.12
+#
+#
+# description
+# 
+# imports
+
+# static variables
+
+# module
+    
+    
+if __name__ == '__main__':
+    name_me()
+
+
+
